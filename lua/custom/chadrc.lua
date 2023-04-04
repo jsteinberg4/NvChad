50685d38c6b2b97033fc7dc0170f2ff20406a1ba
Jesse Steinberg <jessesteinberg01@gmail.com>
2023-04-04 17:21:45 -0400

    track custom modules

---
+++ lua/custom/chadrc.lua
@@ -0,0 +1,8 @@
+-- This file overwrites the default configuration options for NVCha
+-- This youtube video explains configuration very well:
+-- https://www.youtube.com/watch?v=Mtgo-nP_r8Y
+local M = {}
+ M.ui = {theme = 'doomchad'}
+-- Add any additional plugins here as a table
+M.plugins = "custom.plugins"
+return M
---
+++ lua/custom/plugins.lua
@@ -0,0 +1,49 @@
+-- This file adds any additional plugins
+-- Plugin definitions follow the folke/lazy.nvim spec
+local plugins = {
+  {
+    "williamboman/mason.nvim",
+    priority = 55, -- Force loading before mason-lspconfig
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "pylint",
+        "black",
+        "autoflake",
+        "usort",
+      },
+    },
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    priority = 50, -- Force loading *after* mason.nvim
+    opts = {
+      -- automatic_installation = true,
+      ensure_installed = {
+        "lua_ls", -- "lua-language-server"
+        "pyright",
+      },
+    },
+    cmd = { "Mason", "MasonInstall", "MasonInstallAll", "MasonUninstall", "MasonUninstallAll", "MasonLog" },
+  },
+  -- { -- surround operators
+  --   "echasnovski/mini.surround",
+  --   -- TODO : Keybindings
+  --   lazy = false,
+  --   keys = {},
+  --   config = function(_, opts)
+  --     require("mini.surround").setup(opts)
+  --   end,
+  -- },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      -- From: https://nvchad.com/docs/config/lsp
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+
+}
+
+return plugins
